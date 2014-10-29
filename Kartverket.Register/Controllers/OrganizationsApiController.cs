b10929810e23ecda8052a34d962f414378c1f95d
Henning Jensen <henning@arkitektum.no>
2014-10-29 14:11:48 +0100

    Implement API method getOrganizationByName

--- Kartverket.Register/Controllers/OrganizationsApiController.cs
+++ Kartverket.Register/Controllers/OrganizationsApiController.cs
@@ -1,14 +1,37 @@
 ﻿using System.Web.Http;
+using Kartverket.Register.Models.Api;
+using Kartverket.Register.Services;
 
 namespace Kartverket.Register.Controllers
 {
     public class OrganizationsApiController : ApiController
     {
+        private readonly IOrganizationService _organizationService;
 
-        public IHttpActionResult GetOrganizationByName(string name)
+        public OrganizationsApiController(IOrganizationService organizationService)
         {
-            return Ok();
+            _organizationService = organizationService;
         }
 
+        [Route("api/organisasjon/navn/{name}")]
+        public IHttpActionResult GetOrganizationByName(string name)
+        {
+            Models.Organization organization = _organizationService.GetOrganizationByName(name);
+
+            if (organization == null)
+                return NotFound();
+
+            return Ok(Convert(organization));
+        }
+
+        private Organization Convert(Models.Organization organization)
+        {
+            return new Organization
+            {
+                Name = organization.Name,
+                Number = organization.Number,
+                LogoUrl = Url.Content("~/data/organizations/") + organization.LogoFilename
+            };
+        }
     }
 }

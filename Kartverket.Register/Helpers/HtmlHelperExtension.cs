ec4315cbed20cc3c58d80fe3ded399c91ed91218
Tor Kjetil <tor@arkitektum.no>
2014-12-02 22:20:01 +0100

    changes in userinterface

--- Kartverket.Register/Helpers/HtmlHelperExtension.cs
+++ Kartverket.Register/Helpers/HtmlHelperExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
@@ -33,6 +34,36 @@ namespace Kartverket.Register.Helpers
         public static string ObjektkatalogUrl(this HtmlHelper helper)
         {
             return WebConfigurationManager.AppSettings["ObjektkatalogUrl"];
+        }
+
+        public static string ToUrl(this HtmlHelper helper, string name)
+        {
+            return MakeSeoFriendlyString(name);
+        }
+
+        private static string MakeSeoFriendlyString(string input)
+        {
+            string encodedUrl = (input ?? "").ToLower();
+
+            // replace & with and
+            encodedUrl = Regex.Replace(encodedUrl, @"\&+", "and");
+
+            // remove characters
+            encodedUrl = encodedUrl.Replace("'", "");
+
+            // replace norwegian characters
+            encodedUrl = encodedUrl.Replace("å", "a").Replace("æ", "ae").Replace("ø", "o");
+
+            // remove invalid characters
+            encodedUrl = Regex.Replace(encodedUrl, @"[^a-z0-9]", "-");
+
+            // remove duplicates
+            encodedUrl = Regex.Replace(encodedUrl, @"-+", "-");
+
+            // trim leading & trailing characters
+            encodedUrl = encodedUrl.Trim('-');
+
+            return encodedUrl;
         }
 
     }

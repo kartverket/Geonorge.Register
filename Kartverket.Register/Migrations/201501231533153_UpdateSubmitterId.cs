595032bf3de733fab2b9a42f574717da06ddd850
veronika@arkitektum.no <veronika@arkitektum.no>
2015-01-23 16:37:12 +0100

    Lagt inn migrering for å legge inn submitterid

---
+++ Kartverket.Register/Migrations/201501231533153_UpdateSubmitterId.cs
@@ -0,0 +1,43 @@
+namespace Kartverket.Register.Migrations
+{
+    using Kartverket.Register.Models;
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Net;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Text.RegularExpressions;
+    
+    public partial class UpdateSubmitterId : DbMigration
+    {
+        public override void Up()
+        {
+            RegisterDbContext db = new RegisterDbContext();
+
+            var queryResultsRegister = from r in db.RegisterItems
+                                       where r.submitterId == null
+                                       select r.systemId;
+
+            List<Guid> systIdListe = queryResultsRegister.ToList();
+
+            foreach (Guid item in systIdListe)
+            {
+                RegisterItem registerItem = db.RegisterItems.Find(item);
+
+                string submitterId = "10087020-f17c-45e1-8542-02acbcf3d8a3";
+
+
+                Sql("UPDATE RegisterItems SET submitterId = '" + submitterId + "' WHERE  (systemId = '" + registerItem.systemId.ToString() + "')");
+
+            }
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

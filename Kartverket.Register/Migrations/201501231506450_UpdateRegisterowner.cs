1ff00ad1578c0684f4f544d206610c2a0ae7b1b3
veronika@arkitektum.no <veronika@arkitektum.no>
2015-01-23 16:17:11 +0100

    Migrering

---
+++ Kartverket.Register/Migrations/201501231506450_UpdateRegisterowner.cs
@@ -0,0 +1,44 @@
+namespace Kartverket.Register.Migrations
+{
+    using Kartverket.Register.Models;
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Net;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Text.RegularExpressions;
+    
+    public partial class UpdateRegisterowner : DbMigration
+    {
+        public override void Up()
+        {
+            RegisterDbContext db = new RegisterDbContext();
+
+            var queryResultsRegisteritems = from r in db.Registers
+                                            where r.ownerId == null
+                                            select r.systemId;
+
+            List<Guid> systIdListe = queryResultsRegisteritems.ToList();
+
+            foreach (Guid item in systIdListe)
+            {
+                Register register = db.Registers.Find(item);
+
+                string ownerId = "10087020-f17c-45e1-8542-02acbcf3d8a3";
+
+
+                Sql("UPDATE Registers SET ownerId = '" + ownerId + "' WHERE  (systemId = '" + register.systemId.ToString() + "')");
+
+            }
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

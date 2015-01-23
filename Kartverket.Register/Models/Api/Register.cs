5c969887fbe10f62e10ec1940968ac1d45eba29c
Tor Kjetil <tor@arkitektum.no>
2015-01-23 10:24:16 +0100

    added gneric json api

---
+++ Kartverket.Register/Models/Api/Register.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kartverket.Register.Models.Api
+{
+    public class Register
+    {
+        public string id { get; set; }
+        public string label { get; set; }
+
+        public string contentsummary { get; set; }
+        public string owner { get; set; }
+        public string manager { get; set; }
+        public string controlbody { get; set; }
+        public List<Registeritem> containeditems { get; set; }
+    }
+}
---
+++ Kartverket.Register/Models/Api/Registeritem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kartverket.Register.Models.Api
+{
+    public class Registeritem
+    {
+        public string id { get; set; }
+        public string label { get; set; }
+        public string itemclass { get; set; }
+        public string status { get; set; }
+
+        public string logo { get; set; }
+        public string documentreference { get; set; }
+    }
+}

63199d8d11fb99a5f979a42f950a2ab252f5c191
veronika@arkitektum.no <veronika@arkitektum.no>
2015-01-23 16:25:20 +0100

    Lagt inn migrering som legger inn status ved nullverdi

---
+++ Kartverket.Register/Migrations/201501231522036_UpdateRegisterStatus.cs
@@ -0,0 +1,43 @@
+namespace Kartverket.Register.Migrations
+{
+    using Kartverket.Register.Models;
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Net;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Text.RegularExpressions;
+    
+    public partial class UpdateRegisterStatus : DbMigration
+    {
+        public override void Up()
+        {
+            RegisterDbContext db = new RegisterDbContext();
+
+            var queryResultsRegister = from r in db.Registers
+                                            where r.statusId == null
+                                            select r.systemId;
+
+            List<Guid> systIdListe = queryResultsRegister.ToList();
+
+            foreach (Guid item in systIdListe)
+            {
+                Register register = db.Registers.Find(item);
+
+                string statusId = "Submitted";
+
+
+                Sql("UPDATE Registers SET statusId = '" + statusId + "' WHERE  (systemId = '" + register.systemId.ToString() + "')");
+
+            }
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

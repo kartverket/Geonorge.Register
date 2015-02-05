0de89bd9b6f367cc1edcd66566acf82d63132e9c
veronika@arkitektum.no <veronika@arkitektum.no>
2015-02-05 11:25:44 +0100

    Lagt inn en migrering for å gi alle felt med statusId lik null en verdi

---
+++ Kartverket.Register/Migrations/201502051017299_UpdateStatusId.cs
@@ -0,0 +1,43 @@
+namespace Kartverket.Register.Migrations
+{
+    using Kartverket.Register.Models;
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Net;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Text.RegularExpressions;
+    
+    public partial class UpdateStatusId : DbMigration
+    {
+        public override void Up()
+        {
+            RegisterDbContext db = new RegisterDbContext();
+
+            var queryResultsRegister = from r in db.RegisterItems
+                                       where r.statusId == null
+                                       select r.systemId;
+
+            List<Guid> systIdListe = queryResultsRegister.ToList();
+
+            foreach (Guid item in systIdListe)
+            {
+                RegisterItem registerItem = db.RegisterItems.Find(item);
+
+                string statusId = "Submitted";
+
+
+                Sql("UPDATE RegisterItems SET statusId = '" + statusId + "' WHERE  (systemId = '" + registerItem.systemId.ToString() + "')");
+
+            }
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

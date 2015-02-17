427fa4dc4e6f3f38081d51b556767f81778fcc7e
veronika@arkitektum.no <veronika@arkitektum.no>
2015-02-17 10:26:59 +0100

    Fikset visning av dokumenteier i på detaljsiden

--- Kartverket.Register/Controllers/RegistersController.cs
+++ Kartverket.Register/Controllers/RegistersController.cs
@@ -56,9 +56,16 @@ namespace Kartverket.Register.Controllers
             var queryResultsRegisterItem = from o in db.RegisterItems
                                          where o.seoname == itemname && o.register.seoname == registername
                                          select o.systemId;
-
+            
             Guid systId = queryResultsRegisterItem.First();
             Kartverket.Register.Models.RegisterItem registerItem = db.RegisterItems.Find(systId);
+
+            if (registerItem.register.containedItemClass == "Document") {
+                Kartverket.Register.Models.Document document = db.Documents.Find(systId);
+                ViewBag.documentOwner = document.documentowner.name;
+            }
+
+
             return View(registerItem);
         }
         

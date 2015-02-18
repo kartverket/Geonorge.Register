897ed9f97df8ce95ec398d79a2b64c72b0f50da7
veronika@arkitektum.no <veronika@arkitektum.no>
2015-02-18 09:19:40 +0100

    Lagt inn en testside for epsg med ulike visningstyper

--- Kartverket.Register/Controllers/RegistersController.cs
+++ Kartverket.Register/Controllers/RegistersController.cs
@@ -48,8 +48,7 @@ namespace Kartverket.Register.Controllers
             return View(register);
         }
 
-        // TEST datasetvisning
-
+        // *****************************  TEST datasetvisning
         [Route("register/dataset/{name}/datasetTest")]
         public ActionResult NyDetailsDataset(string name, string sorting, int? page)
         {
@@ -134,7 +133,91 @@ namespace Kartverket.Register.Controllers
 
         }       
 
+        //******************* TEST EPSG visning
+        [Route("register/epsg/{name}/epsgTest")]
+        public ActionResult NyDetailsEPSG(string name, string sorting, int? page)
+        {
 
+            var queryResults = from o in db.Registers
+                               where o.name == name || o.seoname == name
+                               select o.systemId;
+
+            Guid systId = queryResults.First();
+
+            Kartverket.Register.Models.Register register = db.Registers.Find(systId);
+            if (register.containedItemClass == "EPSG")
+            {
+
+                var queryResultsTest = from d in db.EPSGs
+                                       select d.systemId;
+                List<Guid> epsgID = queryResultsTest.ToList();
+                List<EPSG> epsgs = new List<EPSG>();
+
+                ViewBag.page = page;
+                ViewBag.sortOrder = sorting;
+                ViewBag.sorting = new SelectList(db.Sorting.ToList(), "value", "description");
+
+                foreach (Guid item in epsgID)
+                {
+                    Kartverket.Register.Models.EPSG epsg = db.EPSGs.Find(item);
+                    epsgs.Add(epsg);
+
+                }
+                int pageSize = 50;
+                int pageNumber = (page ?? 1);
+
+                epsgs.OrderBy(d => d.name);
+
+                if (sorting == "name_desc")
+                {
+                    return View(epsgs.OrderBy(o => o.name).ToList().ToPagedList(pageNumber, pageSize));
+                }
+                if (sorting == "submitter")
+                {
+                    return View(epsgs.OrderBy(o => o.submitter.name).ToList().ToPagedList(pageNumber, pageSize));
+                }
+                if (sorting == "submitter_desc")
+                {
+                    return View(epsgs.OrderByDescending(o => o.submitter.name).ToList().ToPagedList(pageNumber, pageSize));
+                }
+                else if (sorting == "status")
+                {
+                    return View(epsgs.OrderBy(o => o.description).ToList().ToPagedList(pageNumber, pageSize));
+                }
+                else if (sorting == "status_desc")
+                {
+                    return View(epsgs.OrderByDescending(o => o.description).ToList().ToPagedList(pageNumber, pageSize));
+                }
+                else if (sorting == "dateSubmitted")
+                {
+                    return View(epsgs.OrderBy(o => o.dateSubmitted).ToList().ToPagedList(pageNumber, pageSize));
+                }
+                else if (sorting == "dateSubmitted_desc")
+                {
+                    return View(epsgs.OrderByDescending(o => o.dateSubmitted).ToList().ToPagedList(pageNumber, pageSize));
+                }
+                else if (sorting == "modified")
+                {
+                    return View(epsgs.OrderBy(o => o.modified).ToList().ToPagedList(pageNumber, pageSize));
+                }
+                else if (sorting == "modified_desc")
+                {
+                    return View(epsgs.OrderByDescending(o => o.modified).ToList().ToPagedList(pageNumber, pageSize));
+                }
+                else if (sorting == "dateAccepted")
+                {
+                    return View(epsgs.OrderBy(o => o.dateAccepted).ToList().ToPagedList(pageNumber, pageSize));
+                }
+                else if (sorting == "dateAccepted_desc")
+                {
+                    return View(epsgs.OrderByDescending(o => o.dateAccepted).ToList().ToPagedList(pageNumber, pageSize));
+                }
+
+                return View(epsgs.ToPagedList(pageNumber, pageSize));
+            }
+            return View("Index");
+
+        }   
 
 
 

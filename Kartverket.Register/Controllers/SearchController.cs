0e44d426df232604366a18a1e93d5c128e190796
veronika@arkitektum.no <veronika@arkitektum.no>
2015-02-18 10:45:30 +0100

    Laget en ny filtreringsside for å også kunne sortere på dataseteier

--- Kartverket.Register/Controllers/SearchController.cs
+++ Kartverket.Register/Controllers/SearchController.cs
@@ -37,81 +37,115 @@ namespace Kartverket.Register.Controllers
         }
 
 
-        [Route("register/{registername}/{documentownername}/")]
-        public ActionResult DetailsFilterDocument(string registername, string documentownername, string sorting, int? page)
+        //[Route("register/{registername}/{documentownername}/")]
+        //public ActionResult DetailsFilterDocument(string registername, string documentownername, string sorting, int? page)
+        //{
+
+        //    var queryResultsTest = from d in db.Documents
+        //                          where d.register.seoname == registername && d.documentowner.seoname == documentownername
+        //                          select d.systemId;
+
+        //    List<Guid> documentsId = queryResultsTest.ToList();
+        //    List<Document> documents = new List<Document>();
+
+        //    ViewBag.page = page;
+        //    ViewBag.sortOrder = sorting;
+        //    ViewBag.sorting = new SelectList(db.Sorting.ToList(), "value", "description");
+            
+        //    foreach (Guid item in documentsId)
+        //    {
+        //        Kartverket.Register.Models.Document document = db.Documents.Find(item);
+        //        if (document.documentowner.seoname == documentownername)
+        //        {
+        //            documents.Add(document);
+        //        }                
+        //    }
+        //    int pageSize = 50;
+        //    int pageNumber = (page ?? 1);
+
+        //    documents.OrderBy(d => d.name);
+
+        //    if (sorting == "name_desc")
+        //    {
+        //        return View(documents.OrderBy(o => o.name).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+        //    if (sorting == "submitter")
+        //    {
+        //        return View(documents.OrderBy(o => o.submitter.name).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+        //    if (sorting == "submitter_desc")
+        //    {
+        //        return View(documents.OrderByDescending(o => o.submitter.name).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+        //    else if (sorting == "status")
+        //    {
+        //        return View(documents.OrderBy(o => o.description).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+        //    else if (sorting == "status_desc")
+        //    {
+        //        return View(documents.OrderByDescending(o => o.description).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+        //    else if (sorting == "dateSubmitted")
+        //    {
+        //        return View(documents.OrderBy(o => o.dateSubmitted).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+        //    else if (sorting == "dateSubmitted_desc")
+        //    {
+        //        return View(documents.OrderByDescending(o => o.dateSubmitted).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+        //    else if (sorting == "modified")
+        //    {
+        //        return View(documents.OrderBy(o => o.modified).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+        //    else if (sorting == "modified_desc")
+        //    {
+        //        return View(documents.OrderByDescending(o => o.modified).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+        //    else if (sorting == "dateAccepted")
+        //    {
+        //        return View(documents.OrderBy(o => o.dateAccepted).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+        //    else if (sorting == "dateAccepted_desc")
+        //    {
+        //        return View(documents.OrderByDescending(o => o.dateAccepted).ToList().ToPagedList(pageNumber, pageSize));
+        //    }
+
+            
+        //    return View(documents.ToPagedList(pageNumber, pageSize));
+        //}
+
+        // GET: Registers/Details/5
+        [Route("register/{registername}/{itemOwner}/")]
+        public ActionResult DetailsFilter(string registername, string itemOwner, string sorting, int? page)  //(string registername, string documentownername, string sorting, int? page)
         {
+            var queryResults = from o in db.Registers
+                               where o.name == registername || o.seoname == registername
+                               select o.systemId;
 
-            var queryResultsTest = from d in db.Documents
-                                  where d.register.seoname == registername && d.documentowner.seoname == documentownername
-                                  select d.systemId;
+            var queryResultsOrganization = from o in db.Organizations
+                                           where o.seoname == itemOwner 
+                                           select o.systemId;
 
-            List<Guid> documentsId = queryResultsTest.ToList();
-            List<Document> documents = new List<Document>();
-
+            Guid orgId = queryResultsOrganization.First();
+            Kartverket.Register.Models.Organization organisasjon = db.Organizations.Find(orgId);
+            Guid systId = queryResults.First();
+            Kartverket.Register.Models.Register register = db.Registers.Find(systId);
+            
             ViewBag.page = page;
-            ViewBag.sortOrder = sorting;
+            ViewBag.SortOrder = sorting;
             ViewBag.sorting = new SelectList(db.Sorting.ToList(), "value", "description");
-            
-            foreach (Guid item in documentsId)
-            {
-                Kartverket.Register.Models.Document document = db.Documents.Find(item);
-                if (document.documentowner.seoname == documentownername)
-                {
-                    documents.Add(document);
-                }                
-            }
-            int pageSize = 50;
-            int pageNumber = (page ?? 1);
+            ViewBag.register = register.name;
+            ViewBag.owner = organisasjon.name;
+            ViewBag.ownerLogo = organisasjon.logoFilename;
+            ViewBag.ownerSEO = organisasjon.seoname;
 
-            documents.OrderBy(d => d.name);
+                
 
-            if (sorting == "name_desc")
+            if (register == null)
             {
-                return View(documents.OrderBy(o => o.name).ToList().ToPagedList(pageNumber, pageSize));
+                return HttpNotFound();
             }
-            if (sorting == "submitter")
-            {
-                return View(documents.OrderBy(o => o.submitter.name).ToList().ToPagedList(pageNumber, pageSize));
-            }
-            if (sorting == "submitter_desc")
-            {
-                return View(documents.OrderByDescending(o => o.submitter.name).ToList().ToPagedList(pageNumber, pageSize));
-            }
-            else if (sorting == "status")
-            {
-                return View(documents.OrderBy(o => o.description).ToList().ToPagedList(pageNumber, pageSize));
-            }
-            else if (sorting == "status_desc")
-            {
-                return View(documents.OrderByDescending(o => o.description).ToList().ToPagedList(pageNumber, pageSize));
-            }
-            else if (sorting == "dateSubmitted")
-            {
-                return View(documents.OrderBy(o => o.dateSubmitted).ToList().ToPagedList(pageNumber, pageSize));
-            }
-            else if (sorting == "dateSubmitted_desc")
-            {
-                return View(documents.OrderByDescending(o => o.dateSubmitted).ToList().ToPagedList(pageNumber, pageSize));
-            }
-            else if (sorting == "modified")
-            {
-                return View(documents.OrderBy(o => o.modified).ToList().ToPagedList(pageNumber, pageSize));
-            }
-            else if (sorting == "modified_desc")
-            {
-                return View(documents.OrderByDescending(o => o.modified).ToList().ToPagedList(pageNumber, pageSize));
-            }
-            else if (sorting == "dateAccepted")
-            {
-                return View(documents.OrderBy(o => o.dateAccepted).ToList().ToPagedList(pageNumber, pageSize));
-            }
-            else if (sorting == "dateAccepted_desc")
-            {
-                return View(documents.OrderByDescending(o => o.dateAccepted).ToList().ToPagedList(pageNumber, pageSize));
-            }
-
-            
-            return View(documents.ToPagedList(pageNumber, pageSize));
-        }       
+            return View(register);
+        }
     }
 }

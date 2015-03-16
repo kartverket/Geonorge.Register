d79a10b28bea1c7f5d45ea602857451eebce2597
veronika@arkitektum.no <veronika@arkitektum.no>
2015-03-16 10:29:35 +0100

    Lagt inn migreringer for å slette metadata kodelister

---
+++ Kartverket.Register/Migrations/201503160928263_DeleteMetadataKodelister.cs
@@ -0,0 +1,17 @@
+namespace Kartverket.Register.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteMetadataKodelister : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("DELETE FROM Registers WHERE (systemId = '9A46038D-16EE-4562-96D2-8F6304AAB689')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

efbca48cca794e34e8a9b71639b1d756676ee2cb
veronika@arkitektum.no <veronika@arkitektum.no>
2015-03-16 09:45:45 +0100

    Fikset migrering for å slette metadatakodelister

--- Kartverket.Register/Migrations/201503141124220_deleteMetadataKodelister.cs
+++ Kartverket.Register/Migrations/201503141124220_deleteMetadataKodelister.cs
@@ -16,37 +16,39 @@ namespace Kartverket.Register.Migrations
     {
         public override void Up()
         {
-            //RegisterDbContext db = new RegisterDbContext();
+            RegisterDbContext db = new RegisterDbContext();
 
-            //var queryResultsRegisterItem = from r in db.RegisterItems
-            //                               where r.register.parentRegister.name == "Metadata kodelister"
-            //                               select r.systemId;
+            var queryResultsRegisterItem = from r in db.RegisterItems
+                                           where r.register.parentRegisterId.ToString() == "9A46038D-16EE-4562-96D2-8F6304AAB689"
+                                           select r.systemId;
 
-            //List<Guid> registeritems = queryResultsRegisterItem.ToList();
+            List<Guid> registeritems = queryResultsRegisterItem.ToList();
 
-            //foreach (Guid item in registeritems)
-            //{
-            //    //RegisterItem registerItem = db.RegisterItems.Find(item);
+            foreach (Guid item in registeritems)
+            {
+                //RegisterItem registerItem = db.RegisterItems.Find(item);
 
-            //    Sql("DELETE RegisterItems WHERE (systemId = '" + item + "')");
+                Sql("DELETE RegisterItems WHERE (systemId = '" + item + "')");
 
-            //}
+            }
 
-            //var queryResultsRegister = from r in db.Registers
-            //                           where r.parentRegister.name == "Metadata kodelister" || r.name == "Metadata kodelister"
-            //                           select r.systemId;
+            var queryResultsRegister = from r in db.Registers
+                                       where r.parentRegisterId.ToString() == "9A46038D-16EE-4562-96D2-8F6304AAB689" || r.systemId.ToString() == "9A46038D-16EE-4562-96D2-8F6304AAB689"
+                                       select r.systemId;
 
-            //List<Guid> register = queryResultsRegister.ToList();
-            //foreach (Guid item in register)
-            //{
-            //    //RegisterItem registerItem = db.RegisterItems.Find(item);
+            List<Guid> register = queryResultsRegister.ToList();
+            foreach (Guid item in register)
+            {
+                //RegisterItem registerItem = db.RegisterItems.Find(item);
 
-            //    Sql("DELETE Registers WHERE (systemId = '" + item + "')");
+                Sql("DELETE FROM Registers WHERE (systemId = '" + item + "')");
 
-            //}
+            }
 
-        }
-        
+                Sql("DELETE FROM Registers WHERE (systemId = '9A46038D-16EE-4562-96D2-8F6304AAB689')");
+       }
+
+       
         public override void Down()
         {
         }

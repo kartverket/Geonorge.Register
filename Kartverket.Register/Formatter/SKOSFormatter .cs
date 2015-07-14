81fabcc36582290f08490eb2ab8f421fff7ef437
veronika@arkitektum.no <veronika@arkitektum.no>
2015-07-14 08:00:59 +0200

    feil fix sist commit

--- Kartverket.Register/Formatter/SKOSFormatter .cs
+++ Kartverket.Register/Formatter/SKOSFormatter .cs
@@ -72,7 +72,7 @@ namespace Kartverket.Register.Formatter
                         new XElement(skosNs + "prefLabel", conceptSheme.name, new XAttribute(XNamespace.Xml + "lang", "no")),
                         new XElement(dctermsNs + "description", conceptSheme.description, new XAttribute(XNamespace.Xml + "lang", "no")),
                         new XElement(dctermsNs + "source", new XAttribute(rdfNs + "resource", conceptSheme.id)),
-                        new XElement(skosNs + "broader", new XAttribute(rdfNs + "resource", conceptSheme.broader)),  
+                        new XElement(skosNs + "broader", new XAttribute(rdfNs + "resource", conceptSheme.broader))
                         //new XElement(skosNs + "broaderTransitive", new XAttribute(rdfNs + "resource", conceptSheme.broader)) 
                         ),
 
@@ -83,7 +83,7 @@ namespace Kartverket.Register.Formatter
                         new XElement(skosNs + "prefLabel", c.name, new XAttribute(XNamespace.Xml + "lang", "no")),
                         new XElement(dctermsNs + "description", c.codevalue, new XAttribute(XNamespace.Xml + "lang", "no")),                
                         new XElement(dctermsNs + "source", new XAttribute(rdfNs + "resource", c.id)),
-                        new XElement(skosNs + "broader", new XAttribute(rdfNs + "resource", c.broader)),
+                        new XElement(skosNs + "broader", new XAttribute(rdfNs + "resource", c.broader))
                         //new XElement(skosNs + "broaderTransitive", new XAttribute(rdfNs + "resource", c.broader)) 
                       ));
 

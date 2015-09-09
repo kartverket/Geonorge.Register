3f60fb4663433e598e66579c72070c14782ed5ca
veronika@arkitektum.no <veronika@arkitektum.no>
2015-09-09 15:37:42 +0200

    Fikset CSV formatter til å ta imot register og registeritems

--- Kartverket.Register/Formatter/CsvFormatter.cs
+++ Kartverket.Register/Formatter/CsvFormatter.cs
@@ -6,29 +6,38 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace Kartverket.Register.Formatter
 {
     public class CsvFormatter : BufferedMediaTypeFormatter
     {
+        private readonly string csv = "text/csv";
+
         public CsvFormatter()
         {
-            // Add the supported media type.
-            SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/csv"));
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue(csv));
+            MediaTypeMappings.Add(new UriPathExtensionMapping("csv", csv));
         }
+
+        Func<Type, bool> SupportedTypeCSV = (type) =>
+        {
+            if (type == typeof(Kartverket.Register.Models.Api.Register) ||
+                type == typeof(IEnumerable<Kartverket.Register.Models.Api.Register>) ||
+                type == typeof(List<Kartverket.Register.Models.Api.Register>) ||
+                type == typeof(IEnumerable<Kartverket.Register.Models.Api.Registeritem>) ||
+                type == typeof(List<Kartverket.Register.Models.Api.Registeritem>))
+
+                return true;
+            else
+                return false;
+        };
 
         public override bool CanWriteType(System.Type type)
         {
-            if (type == typeof(Item))
-            {
-                return true;
-            }
-            else
-            {
-                Type enumerableType = typeof(IEnumerable<Item>);
-                return enumerableType.IsAssignableFrom(type);
-            }
+            return SupportedTypeCSV(type);
         }
         public override bool CanReadType(Type type)
         {
@@ -37,50 +46,94 @@ namespace Kartverket.Register.Formatter
 
         public override void WriteToStream(Type type, object value, Stream writeStream, HttpContent content)
         {
-            using (var writer = new StreamWriter(writeStream))
+            if (type == typeof(Kartverket.Register.Models.Api.Register) ||
+                type == typeof(IEnumerable<Kartverket.Register.Models.Api.Register>) ||
+                type == typeof(List<Kartverket.Register.Models.Api.Register>) ||
+                type == typeof(IEnumerable<Kartverket.Register.Models.Api.Registeritem>) ||
+                type == typeof(List<Kartverket.Register.Models.Api.Registeritem>))
+                BuildCSV(value, writeStream, content.Headers.ContentType.MediaType);
+        }
+
+        private void BuildCSV(object models, Stream stream, string contenttype)
+        {
+            StreamWriter streamWriter = new StreamWriter(stream);
+            if (models is Models.Api.Register)
             {
-                var products = value as IEnumerable<Item>;
-                if (products != null)
+                Models.Api.Register register = (Models.Api.Register)models;
+                ConvertRegisters(streamWriter, register);
+            }
+            if (models is Models.Api.Registeritem)
+            {
+                streamWriter.Write(RegisterItemHeading());
+                Models.Api.Registeritem registerItem = (Models.Api.Registeritem)models;
+                ConvertRegisterItemToCSV(streamWriter, registerItem);
+            }
+            if (models is List<Models.Api.Registeritem>)
+            {
+                streamWriter.Write(RegisterItemHeading());
+                List<Models.Api.Registeritem> registerItems = (List<Models.Api.Registeritem>)models;
+                ConvertRegisterItems(streamWriter, registerItems);
+            }
+        }
+
+        private StringBuilder ConvertRegisterItems(StreamWriter streamWriter, List<Models.Api.Registeritem> registerItemList)
+        {
+            StringBuilder text = new StringBuilder(RegisterItemHeading());                
+
+            foreach (Models.Api.Registeritem item in registerItemList)
+            {
+                ConvertRegisterItemToCSV(streamWriter, item);
+            }
+            return text;
+        }
+
+        private void ConvertRegisters(StreamWriter streamWriter, Models.Api.Register register)
+        {
+            if (register.containeditems != null)
+            {
+                streamWriter.Write(RegisterItemHeading());
+                foreach (Models.Api.Registeritem item in register.containeditems.ToList())
                 {
-                    foreach (var product in products)
-                    {
-                        WriteItem(product, writer);
-                    }
+                    ConvertRegisterItemToCSV(streamWriter, item);
                 }
-                else
+            }
+            else if (register.containedSubRegisters != null)
+            {
+                streamWriter.Write(RegisterHeading());
+                foreach (Models.Api.Register item in register.containedSubRegisters.ToList())
                 {
-                    var singleProduct = value as Item;
-                    if (singleProduct == null)
-                    {
-                        throw new InvalidOperationException("Cannot serialize type");
-                    }
-                    WriteItem(singleProduct, writer);
+                    ConvertRegisterToCSV(streamWriter, item);
                 }
             }
         }
 
-        // Helper methods for serializing Products to CSV format. 
-        private void WriteItem(Item product, StreamWriter writer)
+        private static void ConvertRegisterItemToCSV(StreamWriter streamWriter, Models.Api.Registeritem item)
         {
-            writer.WriteLine("{0},{1},{2},{3}", Escape(product.showUrl),
-                Escape(product.name), Escape(product.description), Escape(product.status));
+            item.description = RemoveBreaksFromDescription(item.description);
+            streamWriter.Write(item.id + ";" + item.label + ";" + item.itemclass + ";" + item.status + ";" + item.description + ";" + item.owner + ";" + item.lastUpdated.ToString("dd/MM/yyyy") + "\n");
         }
 
-        static char[] _specialChars = new char[] { ',', '\n', '\r', '"' };
-
-        private string Escape(object o)
+        private static void ConvertRegisterToCSV(StreamWriter streamWriter, Models.Api.Register register)
         {
-            if (o == null)
-            {
-                return "";
-            }
-            string field = o.ToString();
-            if (field.IndexOfAny(_specialChars) != -1)
-            {
-                // Delimit the entire field with quotes and replace embedded quotes with "".
-                return String.Format("\"{0}\"", field.Replace("\"", "\"\""));
-            }
-            else return field;
+            register.contentsummary = RemoveBreaksFromDescription(register.contentsummary);
+            streamWriter.Write(register.id + ";" + register.label + ";" + register.contentsummary + ";" + register.owner + ";" + register.lastUpdated.ToString("dd/MM/yyyy") + "\n");
+        }
+
+        private static string RemoveBreaksFromDescription(string description)
+        {
+            string replaceWith = " ";
+            description = description.Replace("\r\n", replaceWith).Replace("\n", replaceWith).Replace("\r", replaceWith);
+            return description;
+        }
+
+        private string RegisterItemHeading()
+        {
+            return "Id ;Navn; Type; Status; Beskrivelse; Eier; Oppdatert\n";
+        }
+
+        private string RegisterHeading()
+        {
+            return "Id ;Navn; Beskrivelse; Eier; Oppdatert\n";
         }
     }
 }

0bafc6f119582826bb75d8b4e23ae18bfe69bd3a
Dag Olav <dagolav@arkitektum.no>
2015-12-02 11:02:24 +0100

    Remove reference to local css

--- Kartverket.Register/App_Start/BundleConfig.cs
+++ Kartverket.Register/App_Start/BundleConfig.cs
@@ -8,17 +8,18 @@ namespace Kartverket.Register
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+ 
             //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
             //            "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui")
-                .Include("~/Scripts/jquery-ui-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryui")
+            //    .Include("~/Scripts/jquery-ui-{version}.js"));
 
-            bundles.Add(new StyleBundle("~/Content/jqueryui")
-               .Include("~/Content/themes/base/all.css"));
+            //bundles.Add(new StyleBundle("~/Content/jqueryui")
+            //   .Include("~/Content/themes/base/all.css"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -29,19 +30,20 @@ namespace Kartverket.Register
                       //"~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                     //"~/Content/bootstrap.css",
-                     //"~/Content/common.css",
-                     //"~/Content/navbar.css",
-                     //"~/Content/searchbar.css",
-                     //"~/Content/statusIcons.css",
-                     //"~/Content/print.css",
-                     //"~/Content/mobil.css",
-                     //"~/Content/custom.css",
-                     //"~/Content/chosen.css",
-                     //"~/Content/font-awesome.css",
-                     //"~/Content/jquery-ui.css",
-                     "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //         //"~/Content/bootstrap.css",
+            //         //"~/Content/common.css",
+            //         //"~/Content/navbar.css",
+            //         //"~/Content/searchbar.css",
+            //         //"~/Content/statusIcons.css",
+            //         //"~/Content/print.css",
+            //         //"~/Content/mobil.css",
+            //         //"~/Content/custom.css",
+            //         //"~/Content/chosen.css",
+            //         //"~/Content/font-awesome.css",
+            //         //"~/Content/jquery-ui.css",
+            //         "~/Content/site.css"  
+            //        ));
 
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                 "~/Scripts/visninger.js",

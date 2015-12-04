66959b4425bda7cfcf61bd4b65a56cb87fc7b1ea
Tor Kjetil <tor@arkitektum.no>
2015-12-04 17:32:23 +0100

    deleted files not in use

--- Kartverket.Register/App_Start/BundleConfig.cs
+++ Kartverket.Register/App_Start/BundleConfig.cs
@@ -9,17 +9,12 @@ namespace Kartverket.Register
         public static void RegisterBundles(BundleCollection bundles)
         {
  
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
-
+           
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui")
                 .Include("~/Scripts/jquery-ui-{version}.js"));
-
-            //bundles.Add(new StyleBundle("~/Content/jqueryui")
-            //   .Include("~/Content/themes/base/all.css"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -30,20 +25,7 @@ namespace Kartverket.Register
                       //"~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //         //"~/Content/bootstrap.css",
-            //         //"~/Content/common.css",
-            //         //"~/Content/navbar.css",
-            //         //"~/Content/searchbar.css",
-            //         //"~/Content/statusIcons.css",
-            //         //"~/Content/print.css",
-            //         //"~/Content/mobil.css",
-            //         //"~/Content/custom.css",
-            //         //"~/Content/chosen.css",
-            //         //"~/Content/font-awesome.css",
-            //         //"~/Content/jquery-ui.css",
-            //         "~/Content/site.css"  
-            //        ));
+           
 
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                 "~/Scripts/visninger.js",

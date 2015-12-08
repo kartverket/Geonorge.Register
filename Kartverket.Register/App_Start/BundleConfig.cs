ff8d59eadb7b95028d10a5d78b4c54227251e045
Henning Jensen <henning.jensen@gmail.com>
2015-12-08 13:00:18 +0100

    Cleanup js and css bundles

--- Kartverket.Register/App_Start/BundleConfig.cs
+++ Kartverket.Register/App_Start/BundleConfig.cs
@@ -1,42 +1,18 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Kartverket.Register
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
- 
-           
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui")
-                .Include("~/Scripts/jquery-ui-{version}.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      //"~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/chosen.css",
-                "~/Content/custom.css"                
-                ));
-            
+                "~/Content/custom.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                 "~/Scripts/visninger.js",
-                "~/Scripts/chosen.jquery.js",
                 "~/Scripts/tabs.js"));
 
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
         }
     }

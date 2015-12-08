214bed016be013ca8609ef9f41953bd6d8bc7aa1
Henning Jensen <henning.jensen@gmail.com>
2015-12-08 14:56:12 +0100

    Use leaflet in dataset coverage view.

--- Kartverket.Register/App_Start/BundleConfig.cs
+++ Kartverket.Register/App_Start/BundleConfig.cs
@@ -7,13 +7,14 @@ namespace Kartverket.Register
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/leaflet.css",
                 "~/Content/custom.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                 "~/Scripts/visninger.js",
-                "~/Scripts/tabs.js"));
-
-            BundleTable.EnableOptimizations = true;
+                "~/Scripts/tabs.js",
+                "~/Scripts/leaflet.js",
+                "~/Scripts/L.TileLayer.Kartverket.min.js"));
         }
     }
 }

70fb017c81084695e96bfc367cf19b6270ce353c
Henning Jensen <henning.jensen@gmail.com>
2015-12-09 15:46:54 +0100

    Add utility project for fetching municipality code and organization number.

---
+++ UtilityConsole/MunicipalityCodeAndOrganizationNumber.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UtilityConsole
+{
+    public class MunicipalityCodeAndOrganizationNumber
+    {
+
+        private readonly Dictionary<string, Municipality> _municipalities = new Dictionary<string, Municipality>();
+
+        public void FetchData(string url)
+        {
+            var rawJson = DownloadData(url);
+
+            JObject jsonData = JObject.Parse(rawJson);
+
+            var municipalities = from m in jsonData["data"]
+                                    select new Municipality((string)m["navn"], (string)m["forretningsadresse"]["kommunenummer"], (string)m["organisasjonsnummer"]);
+
+            foreach (Municipality municipality in municipalities)
+            {
+                _municipalities.Add(municipality.Code, municipality);
+
+                Console.WriteLine(municipality.Name + " [" + municipality.Code + "] - " + municipality.OrganizationNumber);
+            }
+
+
+            var links = from link in jsonData["links"] select new {rel = (string) link["rel"], href = (string) link["href"]};
+
+            foreach (var link in links)
+            {
+                Console.WriteLine(link.rel + ": " + link.href);
+                if (link.rel.Equals("next"))
+                {
+                    Console.WriteLine("-- Going to next page");
+                    FetchData(link.href);
+                }
+            }
+
+            
+        }
+
+        public Dictionary<string, Municipality> GetMunicipalities()
+        {
+            return _municipalities;
+        }
+
+
+        private static string DownloadData(string url)
+        {
+            var httpClient = new HttpClient();
+            Task<string> downloadTask = httpClient.GetStringAsync(url);
+            string rawJson = downloadTask.Result;
+            return rawJson;
+        }
+    }
+
+
+    public class Municipality
+    {
+        public Municipality(string name, string code, string organizationNumber)
+        {
+            Name = name;
+            Code = code;
+            OrganizationNumber = organizationNumber;
+        }
+
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string OrganizationNumber { get; set; }
+    }
+}
---
+++ UtilityConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace UtilityConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string url = "http://data.brreg.no/enhetsregisteret/enhet.json?page=0&size=100&$filter=organisasjonsform%20eq%20%27KOMM%27";
+
+            var service = new MunicipalityCodeAndOrganizationNumber();
+            service.FetchData(url);
+
+            Dictionary<string, Municipality> municipalitiesByCode = service.GetMunicipalities();
+            Console.WriteLine(municipalitiesByCode.Keys.Count + " municipalities");
+
+            string fileName = "c:\\dev\\MunicipalityCodeToOrganizationNumber.cs";
+            var fs = new FileStream(fileName, FileMode.CreateNew);
+            using (StreamWriter file = new StreamWriter(fs))
+            {
+                file.WriteLine("Dictionary<string, string> municipalityFromCodeToOrganizationNumber = new Dictionary<string, string>() {");
+
+                foreach (string key in municipalitiesByCode.Keys)
+                {
+                    file.WriteLine("{\"" + key + "\",\"" + municipalitiesByCode[key].OrganizationNumber + "\"},");
+                }
+
+                file.WriteLine("};");
+            }
+        }
+    }
+}

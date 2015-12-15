3d0170f6542999370f0ac93b98f478a7bf0d5e88
veronika@arkitektum.no <veronika@arkitektum.no>
2015-12-15 09:34:49 +0100

    Fix test PostCreateShouldReturnViewWhenUuidIsNotNull

--- Kartverket.Register.Tests/Controllers/DatasetsControllerTest.cs
+++ Kartverket.Register.Tests/Controllers/DatasetsControllerTest.cs
@@ -83,7 +83,7 @@ namespace Kartverket.Register.Tests.Controllers
             accessControlService.Setup(a => a.Access(It.IsAny<Dataset>())).Returns(true);
             registerItemService.Setup(v => v.validateName(It.IsAny<Dataset>())).Returns(true);
             accessControlService.Setup(a => a.GetSecurityClaim("organization")).Returns(dataset.submitter.seoname);
-            registerService.Setup(o => o.GetOrganization()).Returns(dataset.submitter);
+            registerService.Setup(o => o.GetOrganizationByUserName()).Returns(dataset.submitter);
 
             var controller = CreateController(registerService.Object, registerItemService.Object, accessControlService.Object);
             var result = controller.Create(dataset, dataset.register.seoname, null, null, null) as ActionResult;

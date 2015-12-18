8e58db067d9f13af1090c07002abd3eb00475cb6
veronika@arkitektum.no <veronika@arkitektum.no>
2015-12-18 10:55:35 +0100

    Add display name to CoverageDataset model and fix dok-status

--- Kartverket.Register/Models/CoverageDataset.cs
+++ Kartverket.Register/Models/CoverageDataset.cs
@@ -9,19 +9,24 @@ namespace Kartverket.Register.Models
         [Key]
         public Guid CoverageId { get; set; }
 
+        [Display(Name = "Kommune")]
         [ForeignKey("Municipality")]
         public Guid MunicipalityId { get; set; }
         public virtual Organization Municipality { get; set; }
 
+        [Display(Name = "Bekreftet")]
         public bool ConfirmedDok { get; set; }
 
         [ForeignKey("dataset")]
+        [Display(Name = "Datasett")]
         public Guid DatasetId { get; set; }
         public virtual Dataset dataset { get; set; }
 
+        [Display(Name = "Merknad")]
         public string Note { get; set; }
 
         [ForeignKey("CoverageDOKStatus")]
+        [Display(Name = "DOK-status")]
         public string CoverageDOKStatusId { get; set; }
         public virtual DokStatus CoverageDOKStatus { get; set; }
     }

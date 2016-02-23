a26b7245deb96575ba5eb0230321f6b13b7b05e7
veronika@arkitektum.no <veronika@arkitektum.no>
2016-02-23 20:28:28 +0100

    Add view and view model for creating municipal datasets

---
+++ Kartverket.Register/Models/ViewModels/CreateDokMunicipalViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kartverket.Register.Models.ViewModels
+{
+    public class CreateDokMunicipalViewModel
+    {
+        public List<MetadataItemViewModel> SelectedList { get; set; }
+        public List<MetadataItemViewModel> SearchResult { get; set; }
+        public Organization DatasetOwner { get; set; }
+        public Register Register { get; set; }
+        public string MunicipalityCode { get; set; }
+
+        public CreateDokMunicipalViewModel() {
+            //SelectedList = new List<MetadataItemViewModel>();
+            //SearchResult = new List<MetadataItemViewModel>();
+        }
+
+    }
+}

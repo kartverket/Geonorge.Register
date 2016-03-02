ffa369253d0b9d60d07fd129369ad30df53fd928
veronika@arkitektum.no <veronika@arkitektum.no>
2016-03-02 09:17:34 +0100

    Add dialogue to create new service alerts

--- Kartverket.Register/Models/ServiceAlert.cs
+++ Kartverket.Register/Models/ServiceAlert.cs
@@ -1,24 +1,60 @@
-﻿using System;
+﻿using GeoNorgeAPI;
+using Kartverket.DOK.Service;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Configuration;
 
 namespace Kartverket.Register.Models
 {
     public class ServiceAlert : RegisterItem
     {
+        public ServiceAlert()
+        {
+            AlertDate = DateTime.Now;
+            EffectiveDate = DateTime.Now;
+        }
+
+        [Required]
+        [Display(Name = "Varslingsdato:")]
         [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime AlertDate { get; set; }
+
+        [Required]
+        [Display(Name = "Ikrafttredelsesdato:")]
         [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EffectiveDate { get; set; }
+
+        [Required]
+        [Display(Name = "Type varsel:")]
         public string AlertType { get; set; }
+
+        [Required]
+        [Display(Name = "Tjenestetype:")]
         public string ServiceType { get; set; }
+
+        [Required]
+        [Display(Name = "Eier:")]
         [ForeignKey("Owner")]
         public Guid OwnerId { get; set; }
         public virtual Organization Owner { get; set; }
-        public string Note { get; set; }
+
         public string ServiceMetadataUrl { get; set; }
+
+        [Required]
         public string ServiceUuid { get; set; }
+
+        [Display(Name = "Varselet gjelder:")]
+        public string Note { get; set; }
+
+
+        public void GetMetadataByUuid()
+        {
+            SimpleMetadata metadata = MetadataService.FetchMetadata(ServiceUuid);
+            name = metadata.Title;
+            ServiceMetadataUrl = WebConfigurationManager.AppSettings["KartkatalogenUrl"] + "metadata/uuid/" + ServiceUuid;
+        }
 
         public List<string> GetAlertTypes()
         {

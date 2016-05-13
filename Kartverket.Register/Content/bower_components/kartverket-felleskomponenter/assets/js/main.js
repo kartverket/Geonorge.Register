618a31d4c864b74e2cc80b09641c5d3faa567523
Benjamin Dehli <benjamin@arkitektum.no>
2016-05-13 17:40:58 +0200

    Adjust smiley icons

--- Kartverket.Register/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js
+++ Kartverket.Register/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js
@@ -725,6 +725,21 @@ function updateShoppingCartCookie() {
 $(window).load(function () {
     updateShoppingCart();
 });
+$("document").ready( function(){
+    $("a.help-text-toggle").click(function (event) {
+        event.preventDefault();
+        var toggleButton = $(this);
+        var helpTextId = $(this).data("help-text-id");
+        $("#" + helpTextId).toggle();
+        if ($("#" + helpTextId).hasClass('active')) {
+            $("#" + helpTextId).removeClass('active');
+            toggleButton.removeClass('active');
+        } else {
+            $("#" + helpTextId).addClass('active');
+            toggleButton.addClass('active');
+        }
+    });
+});
 function activateTab(tab){
 	$(".link-tabs").ready(function () {
 		tabLink = $(".link-tabs li a[data-tab='" + tab + "']");

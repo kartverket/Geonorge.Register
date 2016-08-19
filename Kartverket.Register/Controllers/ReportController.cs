6779a4cb1ee65317ca8bf7434b5124e05e71c29a
Henning Jensen <henning.jensen@gmail.com>
2016-08-19 09:50:24 +0200

    Upgrade nuget packages.

---
+++ Kartverket.Register/Controllers/ReportController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using Kartverket.ReportApi;
+
+namespace Kartverket.Register.Controllers
+{
+    public class ReportController : ApiController
+    {
+
+        public void Get()
+        {
+            var query = new ReportQuery();
+        }
+
+
+    }
+}

2327265319561b3cd247d4d4e5ef206c7f23bef8
Benjamin Dehli <benjamin@arkitektum.no>
2016-08-29 14:51:25 +0200

    Fix status icon bug in Chrome browsers and update Bower components

--- Kartverket.Register/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js
+++ Kartverket.Register/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js
@@ -731,21 +731,26 @@ function updateShoppingCartCookie() {
 $(window).load(function () {
     updateShoppingCart();
 });
+/* Content toggle */
+function updateToggleLinks(element) {
+	$(element).each(function () {
+		$(this).toggleClass('show-content');
+	});
+}
 $("document").ready( function(){
-    $("a.help-text-toggle").click(function (event) {
-        event.preventDefault();
-        var toggleButton = $(this);
-        var helpTextId = $(this).data("help-text-id");
-        $("#" + helpTextId).toggle();
-        if ($("#" + helpTextId).hasClass('active')) {
-            $("#" + helpTextId).removeClass('active');
-            toggleButton.removeClass('active');
-        } else {
-            $("#" + helpTextId).addClass('active');
-            toggleButton.addClass('active');
-        }
-    });
+
+	updateToggleLinks($('.toggle-content'));
+
+	$(".toggle-content").click(function () {
+		var toggleClass = $(this).data('content-toggle');
+		updateToggleLinks($(this));
+		$("." + toggleClass).toggle();
+	});
+	
 });
+
+
+/* Tabs */
 function activateTab(tab){
 	$(".link-tabs").ready(function () {
 		tabLink = $(".link-tabs li a[data-tab='" + tab + "']");
@@ -760,5 +765,23 @@ $(".link-tabs").ready(function () {
 		activateTab($(this).data('tab'));
 		$("#tab-content").css('opacity', '.15');
 		window.location.href = $(this).prop('href');
+	});
+});
+
+
+/* Help texts */
+$("document").ready( function(){
+	$("a.help-text-toggle").click(function (event) {
+		event.preventDefault();
+		var toggleButton = $(this);
+		var helpTextId = $(this).data("help-text-id");
+		$("#" + helpTextId).toggle();
+		if ($("#" + helpTextId).hasClass('active')) {
+			$("#" + helpTextId).removeClass('active');
+			toggleButton.removeClass('active');
+		} else {
+			$("#" + helpTextId).addClass('active');
+			toggleButton.addClass('active');
+		}
 	});
 });

3a00f77e60b913a02845eef82551a922030522d4
veronika@arkitektum.no <veronika@arkitektum.no>
2017-08-28 11:06:11 +0200

    Add Inspire statusregister

--- Kartverket.Register/Migrations/Configuration.cs
+++ Kartverket.Register/Migrations/Configuration.cs
@@ -109,6 +109,22 @@ namespace Kartverket.Register.Migrations
                 new Models.Dimension { value = "compound", description = "Sammensatt" }
             );
 
+            context.Registers.AddOrUpdate(
+                new Register
+                {
+                    systemId = Guid.Parse("9a9bef28-285b-477e-85f1-504f8227ff45"),
+                    ownerId = Guid.Parse("10087020-F17C-45E1-8542-02ACBCF3D8A3"),
+                    managerId = Guid.Parse("10087020-F17C-45E1-8542-02ACBCF3D8A3"),
+                    name = "Inspire statusregister",
+                    seoname = "inspire-statusregister",
+                    statusId = "Valid",
+                    dateSubmitted = DateTime.Now,
+                    modified = DateTime.Now,
+                    containedItemClass = "InspireDataset",
+                    accessId = 1
+                }
+            );
+
 
             // *** DEFAULT REGISTERS
 

64692742afed0c8616e77842089cc718eb770917
Dag Olav <dagolav@arkitektum.no>
2017-09-29 08:32:56 +0200

    Translation for DOKDeliveryStatus

--- Kartverket.Register/Models/DokDeliveryStatus.cs
+++ Kartverket.Register/Models/DokDeliveryStatus.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using Kartverket.Register.Helpers;
 
 namespace Kartverket.Register.Models
 {
@@ -11,5 +12,15 @@ namespace Kartverket.Register.Models
         [Key]
         public string value { get; set; }
         public string description { get; set; }
+
+        public string DescriptionTranslated()
+        {
+            var cultureName = CultureHelper.GetCurrentCulture();
+            var dokDeliveryStatusDescription = description;
+            if (!CultureHelper.IsNorwegian())
+                dokDeliveryStatusDescription = value;
+
+            return dokDeliveryStatusDescription;
+        }
     }
 }

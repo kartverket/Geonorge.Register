392b4e0fc481d98786293dfa9d2ed026e76a7546
Veronika Andersen <veronika@arkitektum.no>
2018-05-09 12:46:26 +0200

    Add synchronize model

---
+++ Kartverket.Register/Models/Synchronize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kartverket.Register.Models
+{
+    public class Synchronize
+    {
+        public Guid Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Stop { get; set; }
+        public bool Active { get; set; }
+        public int SuccessCount { get; set; }
+        public int FailCount { get; set; }
+        public List<Log> FailLog { get; set; }
+    }
+
+    public class Log
+    {
+        public string Uuid { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}

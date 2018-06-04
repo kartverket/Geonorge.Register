4c74e3e8300fb201316abb34d2228bbacadd476c
Veronika Andersen <veronika@arkitektum.no>
2018-06-04 07:49:24 +0200

    Include vue

--- Kartverket.Register/App_Start/BundleConfig.cs
+++ Kartverket.Register/App_Start/BundleConfig.cs
@@ -17,7 +17,7 @@ namespace Kartverket.Register
             bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/js/scripts").Include(
                "~/Content/bower_components/kartverket-felleskomponenter/assets/js/vendor.min.js",
                "~/Content/bower_components/kartverket-felleskomponenter/assets/js/main.min.js",
-            "~/Content/bower_components/vue/dist/vue.min.js"
+                "~/Content/bower_components/vue/dist/vue.min.js"
            ));
 
 

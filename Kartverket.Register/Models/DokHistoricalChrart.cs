9c2b6db860ab7a15016eaa76fe08ea11d476983f
Veronika Andersen <veronika@arkitektum.no>
2018-06-29 12:46:01 +0200

    Fix failing test

--- Kartverket.Register/Models/DokHistoricalChrart.cs
+++ Kartverket.Register/Models/DokHistoricalChrart.cs
@@ -8,17 +8,17 @@ namespace Kartverket.Register.Models
         public List<string> Labels { get; set; }
 
         //public List<DataPoint> Metadata { get; set; } 
-        public List<int> Metadata { get; set; } 
-        public List<int> Productsheet { get; set; } 
-        public List<int> PresentationRules { get; set; } 
-        public List<int> ProductSpecification { get; set; } 
-        public List<int> Wms { get; set; } 
-        public List<int> Wfs { get; set; } 
-        public List<int> SosiRequirements { get; set; } 
-        public List<int> GmlRequirements { get; set; } 
-        public List<int> AtomFeed { get; set; } 
-        public List<int> Distribution { get; set; } 
-    
+        public List<int> Metadata { get; set; }
+        public List<int> Productsheet { get; set; }
+        public List<int> PresentationRules { get; set; }
+        public List<int> ProductSpecification { get; set; }
+        public List<int> Wms { get; set; }
+        public List<int> Wfs { get; set; }
+        public List<int> SosiRequirements { get; set; }
+        public List<int> GmlRequirements { get; set; }
+        public List<int> AtomFeed { get; set; }
+        public List<int> Distribution { get; set; }
+
 
         public DokHistoricalChart(List<StatusReport> statusReports)
         {
@@ -33,22 +33,25 @@ namespace Kartverket.Register.Models
             AtomFeed = new List<int>();
             Distribution = new List<int>();
             Labels = new List<string>();
-
-            foreach (var statusReport in statusReports)
+            if (statusReports != null)
             {
-                var xName = statusReport.Date.ToString("d MMMM yyyy", CultureInfo.CreateSpecificCulture("nb-NO"));
 
-                Labels.Add(xName);
-                Metadata.Add(statusReport.NumberOfItemsWithMetadata("good"));
-                Productsheet.Add(statusReport.NumberOfItemsWithProductsheet("good"));
-                PresentationRules.Add(statusReport.NumberOfItemsWithPresentationRules("good"));
-                ProductSpecification.Add(statusReport.NumberOfItemsWithProductSpecification("good"));
-                Wms.Add(statusReport.NumberOfItemsWithWms("good"));
-                Wfs.Add(statusReport.NumberOfItemsWithWfs("good"));
-                SosiRequirements.Add(statusReport.NumberOfItemsWithSosiRequirements("good"));
-                GmlRequirements.Add(statusReport.NumberOfItemsWithGmlRequirements("good"));
-                AtomFeed.Add(statusReport.NumberOfItemsWithAtomFeed("good"));
-                Distribution.Add(statusReport.NumberOfItemsWithDistribution("good"));
+                foreach (var statusReport in statusReports)
+                {
+                    var xName = statusReport.Date.ToString("d MMMM yyyy", CultureInfo.CreateSpecificCulture("nb-NO"));
+
+                    Labels.Add(xName);
+                    Metadata.Add(statusReport.NumberOfItemsWithMetadata("good"));
+                    Productsheet.Add(statusReport.NumberOfItemsWithProductsheet("good"));
+                    PresentationRules.Add(statusReport.NumberOfItemsWithPresentationRules("good"));
+                    ProductSpecification.Add(statusReport.NumberOfItemsWithProductSpecification("good"));
+                    Wms.Add(statusReport.NumberOfItemsWithWms("good"));
+                    Wfs.Add(statusReport.NumberOfItemsWithWfs("good"));
+                    SosiRequirements.Add(statusReport.NumberOfItemsWithSosiRequirements("good"));
+                    GmlRequirements.Add(statusReport.NumberOfItemsWithGmlRequirements("good"));
+                    AtomFeed.Add(statusReport.NumberOfItemsWithAtomFeed("good"));
+                    Distribution.Add(statusReport.NumberOfItemsWithDistribution("good"));
+                }
             }
         }
     }

ee0b05b507892ef4c4ee08ac691bb5e7960f6bd2
Dag Olav Dahle <dagolav@arkitektum.no>
2019-08-28 08:33:19 +0200

    Add default sorting alert

--- Kartverket.Register/Services/Register/RegisterService.cs
+++ Kartverket.Register/Services/Register/RegisterService.cs
@@ -123,13 +123,13 @@ namespace Kartverket.Register.Services.Register
         {
             var alerts = register.items.Cast<Alert>();
             if (!string.IsNullOrEmpty(filter.Category) && !string.IsNullOrEmpty(filter.filterOrganization))
-                foreach (Alert item in alerts.Where(a => a.AlertCategory == filter.Category && a.Owner == filter.filterOrganization))
+                foreach (Alert item in alerts.Where(a => a.AlertCategory == filter.Category && a.Owner == filter.filterOrganization).OrderByDescending(o => o.AlertDate))
                     registerItems.Add(item);
             else if (!string.IsNullOrEmpty(filter.Category))
-                foreach (Alert item in alerts.Where(a => a.AlertCategory == filter.Category))
+                foreach (Alert item in alerts.Where(a => a.AlertCategory == filter.Category).OrderByDescending(o => o.AlertDate))
                     registerItems.Add(item);
             else if (!string.IsNullOrEmpty(filter.filterOrganization))
-                foreach (Alert item in alerts.Where(a => a.Owner == filter.filterOrganization))
+                foreach (Alert item in alerts.Where(a => a.Owner == filter.filterOrganization).OrderByDescending(o => o.AlertDate))
                     registerItems.Add(item);
             else
                 foreach (Alert item in alerts)

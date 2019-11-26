acccab213bd47eabba4c88bfa442a9fa3a3e00ca
Dag Olav Dahle <dagolav@arkitektum.no>
2019-11-26 15:15:54 +0100

    MakeAllItemsCodelistRetired

---
+++ Kartverket.Register/Migrations/201911261406473_MakeAllItemsRetired.cs
@@ -0,0 +1,18 @@
+﻿namespace Kartverket.Register.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MakeAllItemsRetired : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Registers", "MakeAllItemsRetired", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Registers", "MakeAllItemsRetired");
+        }
+    }
+}

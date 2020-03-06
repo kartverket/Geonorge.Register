4a5bad127b4c4083ce1dc4919281c6f35e5953d8
Dag Olav Dahle <dagolav@arkitektum.no>
2020-03-06 14:10:26 +0100

    Remove some controller descriptions swagger/help

---
+++ Kartverket.Register/Helpers/CustomDocumentFilter.cs
@@ -0,0 +1,46 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Description;
+
+namespace Kartverket.Register.Helpers
+{
+    public class CustomDocumentFilter : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, System.Web.Http.Description.IApiExplorer apiExplorer)
+        {
+            //make operations alphabetic
+            var paths = swaggerDoc.paths.OrderBy(e => e.Key).ToList();
+            swaggerDoc.paths = paths.ToDictionary(e => e.Key, e => e.Value);
+
+            //controller comments do not get added to swagger docs. This is how to add them.
+            RemoveControllerDescriptions(swaggerDoc, apiExplorer);
+
+        }
+
+        private static void RemoveControllerDescriptions(SwaggerDocument swaggerDoc, System.Web.Http.Description.IApiExplorer apiExplorer)
+        {
+            List<ApiDescription> removeDescriptions = new List<ApiDescription>();
+
+            foreach(var api in apiExplorer.ApiDescriptions)
+            {
+                if (api.ID == "GETapi/ApiSearch" || api.ID == "GETapi/OrganizationsApi" || api.ID.Contains("ApiRoot")
+                    || api.ID.Contains("report") || api.ID.Contains(".{ext}"))
+                    removeDescriptions.Add(api);
+            }
+
+            foreach (var description in removeDescriptions) { 
+                apiExplorer.ApiDescriptions.Remove(description);
+                swaggerDoc.paths.Remove(description.ID);
+            }
+
+        }
+        private static string GetXmlCommentsPath()
+        {
+            return System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/XmlDocument.xml");
+        }
+    }
+}

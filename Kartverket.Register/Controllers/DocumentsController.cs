5f4d258f9348a014b264af14bb07eaffacc3d860
Dag Olav Dahle <dagolav@arkitektum.no>
2022-12-21 11:41:03 +0100

    Fix generate thumbnail

--- Kartverket.Register/Controllers/DocumentsController.cs
+++ Kartverket.Register/Controllers/DocumentsController.cs
@@ -18,11 +18,28 @@ using Kartverket.Register.Services.Translation;
 using Kartverket.Geonorge.Utilities.LogEntry;
 using System.Web.Configuration;
 using Ionic.Zip;
+using Ghostscript.NET.Rasterizer;
+using System.Drawing.Imaging;
 //ghostscriptsharp MIT license:
 //Copyright(c) 2009 Matthew Ephraim
 //Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+//ghostscript GNU Affero General Public License:
+//<Register for documents, generate thumbnail>
+//Copyright (C) 2022, Norwegian Mapping Authority
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Affero General Public License as
+//published by the Free Software Foundation, either version 3 of the
+//License, or (at your option) any later version.
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Affero General Public License for more details.
+//You should have received a copy of the GNU Affero General Public License
+//along with this program.  If not, see https://www.gnu.org/licenses/.
+
 
 namespace Kartverket.Register.Controllers
 {
@@ -438,7 +455,15 @@ namespace Kartverket.Register.Controllers
                 string input = path;
                 string thumbFileName = seofilename.Replace(".pdf", ".jpg");
                 string output = path.Replace(".pdf", ".jpg");
-                GhostscriptWrapper.GenerateOutput(input, output, GsSettings());
+                //GhostscriptWrapper.GenerateOutput(input, output, GsSettings());
+                var rasterizer = new GhostscriptRasterizer();
+                var fileStream = System.IO.File.OpenRead(input);
+                rasterizer.Open(fileStream); //opens the PDF file for rasterizing
+                var pdf2PNG = rasterizer.GetPage(100, 1);
+                pdf2PNG.Save(output, ImageFormat.Png);
+                pdf2PNG.Dispose();
+                rasterizer.Close();
+                fileStream.Close();
 
                 ImageResizer.ImageJob newImage =
                                 new ImageResizer.ImageJob(output, output,

442768e9f08faa6ee1c967bb54800e1011b52988
Dag Olav Dahle <dagolav@arkitektum.no>
2025-02-21 14:20:42 +0100

    Revert "Handle redirect uri"
    This reverts commit f2791a0cd3a0534bdbc15c18741fdad3cb1d7187.

--- Kartverket.Register/Global.asax.cs
+++ Kartverket.Register/Global.asax.cs
@@ -135,17 +135,11 @@ namespace Kartverket.Register
         {
             try
             {
-                var redirectUri = "";
-                if (!string.IsNullOrEmpty(Request.QueryString["ReturnUrl"]))
-                {
-                    redirectUri = Request.QueryString["ReturnUrl"];
-                }
-
                 var loggedInCookie = Context.Request.Cookies["_loggedIn"];
                 if (string.IsNullOrEmpty(Request.QueryString["autologin"]) && loggedInCookie != null && loggedInCookie.Value == "true" && !Request.IsAuthenticated)
                 {
                     if(Request.Path != "/SignOut" && Request.Path != "/signout-callback-oidc" && Request.QueryString["logout"] != "true" && Request.Path != "/shared-partials-scripts" && Request.Path != "/shared-partials-styles" && Request.Path != "/Content/bower_components/kartverket-felleskomponenter/assets/css/styles" && Request.Path != "/Content/local-styles" && Request.Path != "/Content/bower_components/kartverket-felleskomponenter/assets/js/scripts" && Request.Path != "/Scripts/local-scripts")
-                        Response.Redirect("/SignIn?autologin=true&ReturnUrl=" + redirectUri);
+                        Response.Redirect("/SignIn?autologin=true");
                 }
             }
 
